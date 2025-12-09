e4ad6432f71569de320d8afe20a0a3ffa508a221
alfiannurrizky <alfiannrzky0@gmail.com>
2025-12-09 20:59:39 +0700

    first init

---
+++ database/migrations/2025_02_12_182302_add_role_to_users_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRoleToUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id');
+
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('employee_id');
+        });
+    }
+}
